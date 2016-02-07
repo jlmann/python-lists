1a1d10154260186625dd6ca3d1b5ad9b15f390de
joncoop <joncoop1972@gmail.com>
2016-02-07 17:53:29 -0500

    Uploaded initial files. (Need to go through these to make a lesson.)

---
+++ practice_quiz.py
@@ -0,0 +1,67 @@
+# Computer Programming 1
+# Lists Practice Quiz
+#
+# Name: 
+
+"""
+Instructions:
+
+Consider the list below. What is the output of each print statement?
+You can run the program after you answer to check yourself.
+
+"""
+
+pi = [3, 1, 4, 1, 5, 9]
+
+#1
+print( len(pi) )
+
+
+#2
+print( pi[0] )
+
+
+#3
+print( pi[-1] )
+
+
+#4
+print( pi[:3] )
+
+
+#5
+print( pi[2:5] )
+
+
+#6
+print( pi[4:] )
+
+
+
+
+#7 Create a list called 'pals' that contains the first names of 4 friends.  
+      
+   
+
+
+#8 Write a statement that prints a random name from the list you created in 7.
+
+
+   
+
+#9 (2 points)
+
+"""
+Complete the a function definition lucky_7 which returns True if the first
+or last value of nums is a 7 or if the length is 7. Otherwise return False.
+Be sure that the function does not throw an error in the case where an empty 
+list (length zero) is passed in to nums. You should test your function with
+a few different lists.
+"""
+
+def lucky_7(nums):
+    pass
+
+
+
+
